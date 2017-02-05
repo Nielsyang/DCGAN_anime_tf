5534fc04abf95df91a3c7ed64c24d801a4a04b34
CloudYang <yangyun15@lzu.edu.cn>
2017-02-06 06:45:31 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,195 @@
+from __future__ import division
+import math
+import json
+import random
+import pprint
+import scipy.misc
+import numpy as np
+from time import gmtime, strftime
+import tensorflow as tf
+
+def get_image(image_path, img_size, is_grayscale=False):
+  image = imread(image_path, is_grayscale)
+  return transform(image, img_size)
+
+def save_images(images, size, image_path):
+  return imsave(inverse_transform(images), size, image_path)
+
+def imread(path, is_grayscale = False):
+  if (is_grayscale):
+    return scipy.misc.imread(path, flatten = True).astype(np.float)
+  else:
+    return scipy.misc.imread(path).astype(np.float)
+
+#merge all sample_num sample picture into one picture
+def merge(images, size):
+  h, w = images.shape[1], images.shape[2]
+  img = np.zeros((h * size[0], w * size[1], 3))
+  for idx, image in enumerate(images):
+    i = idx % size[1]
+    j = idx // size[1]
+    img[j*h:j*h+h, i*w:i*w+w, :] = image
+  return img
+
+def imsave(images, size, path):
+  return scipy.misc.imsave(path, merge(images, size))
+
+def transform(image, img_size):
+    cropped_image = scipy.misc.imresize(image, [img_size, img_size])
+  return np.array(cropped_image)/127.5 - 1.
+
+def inverse_transform(images):
+  return (images+1.)/2.
+
+def make_gif(images, fname, duration=2, true_image=False):
+  import moviepy.editor as mpy
+
+  def make_frame(t):
+    try:
+      x = images[int(len(images)/duration*t)]
+    except:
+      x = images[-1]
+
+    if true_image:
+      return x.astype(np.uint8)
+    else:
+      return ((x+1)/2*255).astype(np.uint8)
+
+  clip = mpy.VideoClip(make_frame, duration=duration)
+  clip.write_gif(fname, fps = len(images) / duration)
+
+def visualize(sess, dcgan, config, option):
+  if option == 0:
+    z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
+    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+    save_images(samples, [8, 8], './samples/test_%s.png' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+  elif option == 1:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      if config.dataset == "mnist":
+        y = np.random.choice(10, config.batch_size)
+        y_one_hot = np.zeros((config.batch_size, 10))
+        y_one_hot[np.arange(config.batch_size), y] = 1
+
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
+      else:
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+
+      save_images(samples, [8, 8], './samples/test_arange_%s.png' % (idx))
+  elif option == 2:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in [random.randint(0, 99) for _ in xrange(100)]:
+      print(" [*] %d" % idx)
+      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
+      z_sample = np.tile(z, (config.batch_size, 1))
+      #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      if config.dataset == "mnist":
+        y = np.random.choice(10, config.batch_size)
+        y_one_hot = np.zeros((config.batch_size, 10))
+        y_one_hot[np.arange(config.batch_size), y] = 1
+
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
+      else:
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+
+      try:
+        make_gif(samples, './samples/test_gif_%s.gif' % (idx))
+      except:
+        save_images(samples, [8, 8], './samples/test_%s.png' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+  elif option == 3:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
+  elif option == 4:
+    image_set = []
+    values = np.arange(0, 1, 1./config.batch_size)
+
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
+
+      image_set.append(sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample}))
+      make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
+
+    new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
+        for idx in range(64) + range(63, -1, -1)]
+    make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
+
+class batch_norm(object):
+  def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+    with tf.variable_scope(name):
+      self.epsilon  = epsilon
+      self.momentum = momentum
+      self.name = name
+
+  def __call__(self, x, train=True):
+    return tf.contrib.layers.batch_norm(x,
+                      decay=self.momentum, 
+                      updates_collections=None,
+                      epsilon=self.epsilon,
+                      scale=True,
+                      is_training=train,
+                      scope=self.name)
+
+def conv2d(input_, output_dim, 
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="conv2d"):
+  with tf.variable_scope(name):
+    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+              initializer=tf.truncated_normal_initializer(stddev=stddev))
+    conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+
+    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+    conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+    return conv
+
+def deconv2d(input_, output_shape,
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="deconv2d", with_w=False):
+  with tf.variable_scope(name):
+    # filter : [height, width, output_channels, in_channels]
+    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+              initializer=tf.random_normal_initializer(stddev=stddev))
+    
+    deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+              strides=[1, d_h, d_w, 1])
+
+    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+    if with_w:
+      return deconv, w, biases
+    else:
+      return deconv
+     
+def lrelu(x, leak=0.2, name="lrelu"):
+  return tf.maximum(x, leak*x)
+
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+  shape = input_.get_shape().as_list()
+
+  with tf.variable_scope(scope or "Linear"):
+    matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                 tf.random_normal_initializer(stddev=stddev))
+    bias = tf.get_variable("bias", [output_size],
+      initializer=tf.constant_initializer(bias_start))
+    if with_w:
+      return tf.matmul(input_, matrix) + bias, matrix, bias
+    else:
+      return tf.matmul(input_, matrix) + bias
