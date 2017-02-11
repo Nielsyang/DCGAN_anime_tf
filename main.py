177aebc576165ece51b563863b6e98773f9634b6
CloudYang <yangyun15@lzu.edu.cn>
2017-02-11 18:10:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import os
 import scipy.misc
 import numpy as np
 
-from model import DCGAN
-from utils import visualize
+from DCGAN_model import DCGAN
 
 import tensorflow as tf
 
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
-flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
-flags.DEFINE_float("moment", 0.9, "Momentum of MomentumOptimizer")
+flags.DEFINE_integer("epoch", 30, "Epoch to train [25]")
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for d network")
+flags.DEFINE_float("beta1", 0.5, "Momentum of MomentumOptimizer")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
 flags.DEFINE_integer("img_size", 64, "image size")
-flags.DEFINE_integer("c_dim", 3, "Dimension of image color")
+flags.DEFINE_integer("channel_dim", 3, "Dimension of image color")
 flags.DEFINE_string("dataset", "anime", "The name of dataset")
-flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
+flags.DEFINE_string("filename_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
@@ -35,9 +34,9 @@ def main(_):
         sess,
         img_size=FLAGS.img_size,
         batch_size=FLAGS.batch_size,
-        c_dim=FLAGS.c_dim,
+        channel_dim=FLAGS.channel_dim,
         dataset_name=FLAGS.dataset,
-        input_fname_pattern=FLAGS.input_fname_pattern,
+        filename_pattern=FLAGS.filename_pattern,
         checkpoint_dir=FLAGS.checkpoint_dir,
         sample_dir=FLAGS.sample_dir)
 
@@ -46,9 +45,6 @@ def main(_):
     else:
       if not dcgan.load(FLAGS.checkpoint_dir):
         raise Exception("[!] Train a model first, then run test mode")
-      
-    OPTION = 1
-    visualize(sess, dcgan, FLAGS, OPTION)
 
 if __name__ == '__main__':
   tf.app.run()
